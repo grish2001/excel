c1fa19b48a399fc1283d062647eaed667fc63036
grish2001 <80559662+grish2001@users.noreply.github.com>
2022-12-23 12:03:53 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readExcel();
+        }
+
+        private void readExcel()
+        {
+            string FilePath = "C:\\Users\\Студент\\Desktop\\excel\\Выходы211122-291122.xlsx";
+
+            Microsoft.Office.Interop.Excel.Application Excel = new Microsoft.Office.Interop.Excel.Application();
+
+            Workbook wb;
+            Worksheet ws;
+
+            wb = Excel.Workbooks.Open(FilePath);
+            ws = wb.Worksheets[1];
+
+        }
+
+        List<String> user = new List<string>(); 
+
+        
+
+
+
+    }
+}
---
+++ WindowsFormsApp1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string pointOfEntry { get; set; }
+
+    }
+
+
+    List<string> User = new List<string>();
+}
